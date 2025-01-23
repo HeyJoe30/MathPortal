8b6443bc18a0a540142a743a3c06b1fc1f853675
HeyJoe30 <kolotilov1992@gmail.com>
2025-01-23 15:46:45 +0700

    create a model, view and form for publication

--- solvepde/publication/models.py
+++ solvepde/publication/models.py
@@ -1,26 +1,37 @@
 from django.db import models
 from django.conf import settings
+from django.utils import timezone
 
 
 class Publication(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         related_name='publication_created',
         on_delete=models.CASCADE
     )
+
+
     title = models.CharField()
     slug = models.SlugField(max_length=200, blank=True)
-    url = models.URLField(max_length=2000)
-    image = models.ImageField(upload_to='images/%Y/%m/%d/')
     description = models.TextField(blank=True)
-    created_date = models.DateTimeField(auto_now_add=True)
-    published_date = models.DateTimeField(blank=True, null=True)
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=2,
+        choices=Status,
+        default=Status.DRAFT
+    )
 
     class Meta:
         indexes = [
-            models.Index(fields=['-created']),
+            models.Index(fields=['-publish']),
         ]
-        ordering = ['-created']
+        ordering = ['-publish']
 
     def __str__(self):
         return self.title
